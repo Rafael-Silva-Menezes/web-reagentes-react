c5ec2ae1f7f81e96af7e91f70973aa1b4c8a1feb
Felipe-Falcao <felipe.falcao@dcomp.ufs.br>
2021-11-25 22:44:08 -0300

    feat: add globalStyles file

---
+++ src/styles/globals.ts
@@ -0,0 +1,33 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+
+*{
+  margin:0;
+  padding:0;
+  box-sizing:border-box;
+  outline:0;
+}
+
+body{
+  background:#F8F9FF;
+  color: #FFFFFF;
+  -webkit-font-smoothing:antialiased;
+}
+
+
+body, input, button{
+  font-family: sans-serif;
+  font-size: 16px;
+
+}
+
+h1,h2,h3,h4,h5,h6, strong {
+  font-weight: 500;
+}
+
+button{
+  cursor: pointer;
+}
+
+`;
