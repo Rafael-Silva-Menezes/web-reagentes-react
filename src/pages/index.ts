a2478e89b00b4a2448c3f6d19212df0eba59d9ca
Felipe-Falcao <felipe.falcao@dcomp.ufs.br>
2021-12-07 19:32:03 -0300

    feat: add page CadastrarLicenca

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -4,3 +4,4 @@ export { default as CadastroReagentes } from './Reagentes/CadastroReagentes';
 export { default as GerenciarReagentes } from './Reagentes/GerenciarReagentes';
 export { default as Usuarios } from './Usuarios';
 export { default as SolicitacaoCadastro } from './Usuarios/SolicitacaoCadastro';
+export { default as CadastrarLicenca } from './Reagentes/CadastrarLicenca';
