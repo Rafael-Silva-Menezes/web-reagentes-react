9b5f87f061cce7662f39dc55b2b433abccc5cb25
Rafael Silva Menezes <rafasilva320.rs@gmail.com>
2021-12-07 10:10:39 -0300

    feat: ensure AxiosHttpClient calls axios with correct URL

---
+++ src/infra/http/axios-http-client.ts
@@ -0,0 +1,8 @@
+import axios from 'axios';
+import { HttpPostParams } from '@/app/contracts/http';
+
+export class AxiosHttpClient {
+  async post(params: HttpPostParams<any>): Promise<void> {
+    await axios(params.url);
+  }
+}
