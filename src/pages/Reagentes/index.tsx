f794338d67d29e3e99d2ae77467cfcf164efd87f
Felipe-Falcao <felipe.falcao@dcomp.ufs.br>
2021-12-07 19:31:36 -0300

    style: menu buttons

--- src/pages/Reagentes/index.tsx
+++ src/pages/Reagentes/index.tsx
@@ -1,24 +1,23 @@
 import React from 'react';
-import { ReagentesContainer, ReagentesCardButton, ReagentesSpan } from './styles';
-
+import {
+  ReagentesContainer,
+  ReagentesCardButton,
+  ReagentesSpan,
+} from './styles';
 
 const Reagentes: React.FC = ({ children }) => {
-    return (
-        <ReagentesContainer>
-            <ReagentesCardButton href="/cadastro_reagentes">
-                <ReagentesSpan>
-                Cadastrar Reagentes Controlados
-                </ReagentesSpan>
-            </ReagentesCardButton>
-            <ReagentesCardButton href="/gerenciar_reagentes">
-                <ReagentesSpan>
-                Gerenciar Reagentes Controlados
-                </ReagentesSpan>
-            </ReagentesCardButton>
-
-        </ReagentesContainer>
-
-    
-    );
-  };
-  export default Reagentes;
+  return (
+    <ReagentesContainer>
+      <ReagentesCardButton to="/cadastrar_licenca">
+        <ReagentesSpan>Cadastrar Licença</ReagentesSpan>
+      </ReagentesCardButton>
+      <ReagentesCardButton to="/cadastro_reagentes">
+        <ReagentesSpan>Cadastrar Reagentes Controlados</ReagentesSpan>
+      </ReagentesCardButton>
+      <ReagentesCardButton to="/gerenciar_reagentes">
+        <ReagentesSpan>Gerenciar Reagentes Controlados</ReagentesSpan>
+      </ReagentesCardButton>
+    </ReagentesContainer>
+  );
+};
+export default Reagentes;
--- src/pages/Reagentes/styles.ts
+++ src/pages/Reagentes/styles.ts
@@ -1,19 +1,22 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 export const ReagentesContainer = styled.div`
   height: 100%;
-  padding: .1rem;
+  padding: 0.1rem;
   font-weight: 100;
+  display: flex;
+  flex-direction: row;
 `;
 
-export const ReagentesCardButton = styled.a`
+export const ReagentesCardButton = styled(Link)`
   display: inline-block;
   height: 5rem;
   width: 15rem;
   border-radius: 8px;
-  padding: 1rem ;
+  padding: 1rem;
   margin: 1rem;
-  background-color:#081A51;
+  background-color: #081a51;
   color: white;
   text-decoration: none;
   text-align: center;
@@ -22,8 +25,8 @@ export const ReagentesCardButton = styled.a`
 export const ReagentesSpan = styled.span`
   height: 100%;
   font-size: 1.2rem;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
 `;
-
-
-
-
