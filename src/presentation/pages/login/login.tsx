92c5fce4c689c4094a47988733e928b0989af1fd
Rafael Silva Menezes <rafasilva320.rs@gmail.com>
2021-12-08 20:06:22 -0300

    feat: initial setup react render

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
-import Styles from './login-styles.scss';
+// import Styles from './login-styles.scss';
+import { Container } from './styles';
 
 const Login: React.FC = () => {
-  return <div className={Styles.login}>Hello</div>;
+  return (
+    <Container>
+      <form>
+        <input placeholder="email" />
+        <input placeholder="senha" />
+        <button type="submit">Entrar</button>
+      </form>
+    </Container>
+  );
 };
 
 export default Login;
