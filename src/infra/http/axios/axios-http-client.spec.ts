e2f754369cfa1483bb1146493913568a93de9584
Rafael Silva Menezes <rafasilva320.rs@gmail.com>
2021-12-07 21:20:03 -0300

    chore: test eslint

--- src/infra/http/axios/axios-http-client.spec.ts
+++ src/infra/http/axios/axios-http-client.spec.ts
@@ -1,9 +1,9 @@
-import axios from 'axios'
-import { AxiosHttpClient } from './axios-http-client'
-import { mockAxios } from '@/infra/test'
-import { mockPostRequest } from '@/app/test/'
+import axios from 'axios';
+import { AxiosHttpClient } from './axios-http-client';
+import { mockAxios } from '@/infra/test';
+import { mockPostRequest } from '@/app/test/';
 
-jest.mock('axios')
+jest.mock('axios');
 
 type SutTypes = {
   sut: AxiosHttpClient;
@@ -11,25 +11,25 @@ type SutTypes = {
 };
 
 const makeSut = (): SutTypes => {
-  const sut = new AxiosHttpClient()
-  const mockedAxios = mockAxios()
+  const sut = new AxiosHttpClient();
+  const mockedAxios = mockAxios();
   return {
     sut,
-    mockedAxios
-  }
-}
+    mockedAxios,
+  };
+};
 
 describe('AxiosHttpClient', () => {
   test('Should call axios with correct values', async () => {
-    const request = mockPostRequest()
-    const { sut, mockedAxios } = makeSut()
-    await sut.post(request)
-    expect(mockedAxios.post).toHaveBeenCalledWith(request.url, request.body)
-  })
+    const request = mockPostRequest();
+    const { sut, mockedAxios } = makeSut();
+    await sut.post(request);
+    expect(mockedAxios.post).toHaveBeenCalledWith(request.url, request.body);
+  });
 
   test('Should return the correct statusCode and body', () => {
-    const { sut, mockedAxios } = makeSut()
-    const promise = sut.post(mockPostRequest())
-    expect(promise).toEqual(mockedAxios.post.mock.results[0].value)
-  })
-})
+    const { sut, mockedAxios } = makeSut();
+    const promise = sut.post(mockPostRequest());
+    expect(promise).toEqual(mockedAxios.post.mock.results[0].value);
+  });
+});
