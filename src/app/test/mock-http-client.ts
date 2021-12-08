a46ddcc4920c6881a815262546cc17881fa80aac
Rafael Silva Menezes <rafasilva320.rs@gmail.com>
2021-12-07 21:20:55 -0300

    chore: apply eslint

--- src/app/test/mock-http-client.ts
+++ src/app/test/mock-http-client.ts
@@ -7,7 +7,9 @@ import {
 
 export class HttpPostClientSpy<T, R> implements HttpPostClient<T, R> {
   url?: string;
+
   body?: T;
+
   response: HttpResponse<R> = {
     statusCode: HttpStatusCode.ok,
   };
