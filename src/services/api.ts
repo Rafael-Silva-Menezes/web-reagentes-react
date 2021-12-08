291b352b2ab459e928ea839f1db68ed823305dff
Felipe-Falcao <felipe.falcao@dcomp.ufs.br>
2021-12-08 15:05:45 -0300

    refactor: update api path

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,7 @@
-import 'dotenv';
 import axios from 'axios';
 
-export const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+const api = axios.create({
+  baseURL: 'http://reagentes-api.herokuapp.com/api',
 });
+
+export default api;
