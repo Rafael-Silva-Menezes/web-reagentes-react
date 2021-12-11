9feefa92848869866cebd7f6300fd594f7014905
Felipe-Falcao <felipe.falcao@dcomp.ufs.br>
2021-12-11 12:06:24 -0300

    feat: bond request page

--- src/pages/Laboratories/BondRequest/index.tsx
+++ src/pages/Laboratories/BondRequest/index.tsx
@@ -1,161 +1,65 @@
-import React, { useState, useEffect } from 'react';
-import { FiThumbsUp, FiThumbsDown } from 'react-icons/fi';
-import Loader from 'react-loader-spinner';
-import {
-  Container,
-  Table,
-  TextId,
-  TextName,
-  TextNumber,
-  Buttons,
-  Loading,
-  SearchContainer,
-} from './styles';
+import React, { useCallback, useRef } from 'react';
+import { FormHandles } from '@unform/core';
+import { Form } from '@unform/web';
+import { Link, useHistory, useParams } from 'react-router-dom';
+import * as Yup from 'yup';
+import getValidationErrors from '../../../utils/getValidationErrors';
+
+import { path } from '../../../routes';
+import { ParamTypes } from '../../../interfaces/params';
+import { Content, Header } from './styles';
+import Input from '../../../components/Input';
+import Button from '../../../components/Button';
 import Title from '../../../components/Title';
-import ButtonActions from '../../../components/ButtonActions';
-import SearchInput from '../../../components/SearchInput';
-import filterListByText from '../../../utils/filterListByText';
-import api from '../../../services/api';
 
-const content = [
-  {
-    campus: 'Aracaju',
-    department: 'Química',
-    name: 'Química A',
-    code: '1',
-  },
-  {
-    campus: 'Aracaju',
-    department: 'Química',
-    name: 'Química B',
-    code: '2',
-  },
-];
+interface FormData {
+  name: string;
+  code: string;
+}
 
-const BondRequest: React.FC = () => {
-  const [list, setList] = useState<any[]>([]);
-  const [filterList, setFilterList] = useState<any[]>([]);
-  const [call, setCall] = useState(false);
-  const [loading, setLoading] = useState(false);
+const BondRequest: React.FC = ({ children }) => {
+  const formRef = useRef<FormHandles>(null);
+  const { id } = useParams<ParamTypes>();
+  const history = useHistory();
 
-  useEffect(() => {
-    if (list.length === 0 && !call) {
-      // setLoading(true);
-      setList(content);
-      setCall(true);
-      // api
-      //   .post('/users/login', {
-      //     email: 'admin@academico.ufs.br',
-      //     password: 'admin',
-      //   })
-      //   .then(response => {
-      //     api.defaults.headers.common['x-access-token'] =
-      //       response.data.accessToken;
-      //     api.get(`/users/list/pending`).then(listPending => {
-      //       setList(listPending.data);
-      //       setLoading(false);
-      //       setCall(true);
-      //     });
-      //   });
-    }
-  }, [call, list.length]);
+  const handleSubmit = useCallback(
+    async (data: FormData) => {
+      try {
+        formRef.current?.setErrors({});
 
-  const accept = (id: string, name: string): void => {
-    const result = confirm(`Aceitar solicitação de cadastro de ${name}?`);
-    if (result) {
-      // try {
-      //   api.put(`/users/approve/${id}`).then(response => {
-      //     console.log(response);
-      //     if (response.status > 300)
-      //       alert('Não foi possível realizar a operação.');
-      //   });
-      // } catch (err) {
-      //   alert(err);
-      // }
-    }
-  };
+        const schema = Yup.object().shape({
+          name: Yup.string().required('Nome do reagente é obrigatório'),
+          code: Yup.string().required('Código é obrigatório'),
+        });
 
-  const deny = (id: string, name: string): void => {
-    const result = confirm(
-      `Deseja realmente negar a solicitação de cadastro de ${name}?`,
-    );
-    if (result) {
-      // try {
-      //   api.put(`/users/approve/${id}`).then(response => {
-      //     console.log(response);
-      //     if (response.status > 300)
-      //       alert('Não foi possível realizar a operação.');
-      //   });
-      // } catch (err) {
-      //   alert(err);
-      // }
-    }
-  };
+        await schema.validate(data, {
+          abortEarly: false,
+        });
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
-    if (e.target.value === '') setFilterList(list);
-    else
-      setFilterList(
-        filterListByText({
-          list,
-          fieldsToSearch: ['campus', 'department', 'name', 'code'],
-          filter: e.target.value,
-        }),
-      );
-  };
+        //
+
+        history.push(path.laboratories.manage);
+      } catch (err) {
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err);
+          formRef.current?.setErrors(errors);
+        }
+      }
+    },
+    [history],
+  );
 
   return (
     <>
-      <Title>Avaliação de Solicitações de Vínculo</Title>
-      <SearchContainer>
-        <SearchInput
-          type="text"
-          placeholder="Pesquisar"
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-            handleChange(e);
-          }}
-        />
-      </SearchContainer>
-      <Container>
-        {loading ? (
-          <Loading>
-            <Loader
-              type="BallTriangle"
-              color="#081a51"
-              height={80}
-              width={80}
-            />
-          </Loading>
-        ) : (
-          <Table>
-            {list.map(item => (
-              <tr key={item.id}>
-                <TextId>{item.department}</TextId>
-                <TextName>{item.name}</TextName>
-                <TextNumber>Campus {item.campus}</TextNumber>
-                <Buttons>
-                  <div>
-                    <ButtonActions
-                      icon={FiThumbsUp}
-                      onClick={() => {
-                        accept(item.code, item.name);
-                      }}
-                    />
-                    <ButtonActions
-                      color="#081a51"
-                      icon={FiThumbsDown}
-                      onClick={() => {
-                        deny(item.code, item.name);
-                      }}
-                    />
-                  </div>
-                </Buttons>
-              </tr>
-            ))}
-          </Table>
-        )}
-      </Container>
+      <Title>Solicitação de Vínculo a Laboratório</Title>
+      <Content>
+        <Form ref={formRef} onSubmit={handleSubmit}>
+          <Input name="campus" placeholder="Campus" />
+          <Button type="submit">Solicitar</Button>
+        </Form>
+      </Content>
     </>
   );
 };
+
 export default BondRequest;
--- src/pages/Laboratories/BondRequest/styles.ts
+++ src/pages/Laboratories/BondRequest/styles.ts
@@ -1,63 +1,28 @@
 import styled from 'styled-components';
+import { shade } from 'polished';
 
-const primaryColor = '#081a51';
-
-export const Container = styled.div`
-  height: 100%;
-  padding: 0.1rem;
-  margin-top: 3rem;
-  font-weight: 100;
-`;
-
-export const Table = styled.table`
-  width: 90%;
-  border-collapse: separate;
-  border-spacing: 0 1em;
-  margin: auto;
-
-  td {
-    padding: 2px 10px 2px 10px;
-    background-color: #e2e2e2;
-    color: #000;
-  }
-`;
-
-export const TextId = styled.td`
-  font-size: 0.7rem;
-  text-align: left;
-  max-width: 50px;
-  width: auto;
-  min-width: 100px;
-`;
-
-export const TextName = styled.td`
-  font-size: 1.2rem;
-  font-weight: bold;
-  text-align: left;
-`;
-
-export const TextNumber = styled.td`
-  font-size: 0.8rem;
-  text-align: end;
-`;
-
-export const Buttons = styled.td`
-  max-width: 60px;
-  width: auto;
-  min-width: 10px;
-`;
-
-export const Loading = styled.div`
+export const Content = styled.div`
   display: flex;
-  flex-direction: row;
-  margin: 3rem auto auto auto;
+  flex-direction: column;
+  width: 50%;
+  margin: 4rem auto auto auto;
+`;
+
+export const Header = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
-  width: 80%;
-`;
+  margin-top: 30px;
 
-export const SearchContainer = styled.div`
-  display: flex;
-  margin-top: 2rem;
-  width: 30%;
+  a {
+    color: #081a51;
+    align-self: flex-start;
+    margin-left: 40px;
+  }
+
+  a:hover {
+    color: ${shade(0.2, '#081a51')};
+  }
 `;
