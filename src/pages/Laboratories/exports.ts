ba302c664998498185b2b82fbbcc13dc56380d63
Felipe-Falcao <felipe.falcao@dcomp.ufs.br>
2021-12-11 12:05:56 -0300

    refactor: imports exports

--- src/pages/Laboratories/exports.ts
+++ src/pages/Laboratories/exports.ts
@@ -1,3 +1,8 @@
-export * from './index';
-export * from './AddLaboratories';
-export * from './ManageLaboratory';
+export { default as AddLaboratories } from './AddLaboratories';
+export { default as AddRequest } from './AddRequest';
+export { default as EditLaboratory } from './EditLaboratory';
+export { default as ManageAddRequest } from './ManageAddRequest';
+export { default as ManageLaboratory } from './ManageLaboratory';
+export { default as ManageLaboratoryBonds } from './ManageLaboratoryBonds';
+export { default as BondRequest } from './BondRequest';
+export { default as MenuLaboratory } from './index';
--- src/pages/Laboratories/index.tsx
+++ src/pages/Laboratories/index.tsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import { Container, CardButton, Span } from './styles';
+import { path } from '../../routes';
 
 const MenuLaboratory: React.FC = () => {
   return (
     <Container>
-      <CardButton to="/add_laboratory">
+      <CardButton to={path.laboratories.add}>
         <Span>Cadastrar Laboratório</Span>
       </CardButton>
-      <CardButton to="/bond_request">
-        <Span>Avaliar Solicitações de Vínculo</Span>
-      </CardButton>
-      <CardButton to="/manage_laboratory">
+      <CardButton to={path.laboratories.manage}>
         <Span>Gerenciar Laboratórios</Span>
+      </CardButton>
+      <CardButton to={path.laboratories.manageAddRequest}>
+        <Span>Solicitações de Criação de Laboratório</Span>
+      </CardButton>
+      <CardButton to={path.laboratories.bondRequest}>
+        <Span>Solicitar Vínculo a Laboratório</Span>
+      </CardButton>
+      <CardButton to={path.laboratories.addRequest}>
+        <Span>Solicitar Criação de Laboratório</Span>
       </CardButton>
     </Container>
   );
